e1bca8027b4c3f4b96f056293f1cab44d5e5e1e9
archie0732 <121162902+archie0732@users.noreply.github.com>
2026-03-09 14:14:03 +0800

    fix init mnhuagui_list mangas object is undefine

--- src/utils/__init__.ts
+++ src/utils/__init__.ts
@@ -4,6 +4,11 @@ import logger from '../class/logger';
 const dir_path = ['./.setup', './.setup/guilds'];
 const sub_types = ['manhuagui_list.json'];
 
+const manhuagui_list = {
+  mangas: [],
+  last_check_time: '',
+};
+
 export const checkSetup = () => {
   for (const p of dir_path) {
     if (!existsSync(p)) {
@@ -13,7 +18,7 @@ export const checkSetup = () => {
   }
   for (const t of sub_types) {
     if (!existsSync(`./.setup/${t}`)) {
-      writeFileSync(`./.setup/${t}`, JSON.stringify({}, null, 2));
+      writeFileSync(`./.setup/${t}`, JSON.stringify(manhuagui_list, null, 2));
       logger.info(`created file: ${t}`);
     }
   }
